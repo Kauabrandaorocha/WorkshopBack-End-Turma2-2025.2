5cc0a73eac7cd55a96d8f8165ba9b399a8138911
kaua <kauavbrandaorocha@gmail.com>
2025-09-05 16:51:08 -0300

    dia 2 - Fabrica

--- dia2/desafio3.py
+++ dia2/desafio3.py
@@ -2,13 +2,29 @@ import math
 
 class FiguraGeometrica:
 
-    def areaCirculo(raio):
+    def areaCirculo(self, raio):
         areaCirculo = math.pi * math.pow(raio, 2)
         return areaCirculo
     
-    def areaTriangulo(base, altura):
+    def areaTriangulo(self, base, altura):
         areaTriangulo = (base * altura) / 2
         return areaTriangulo
 
-    def hipotenusa(cateto_oposto, cateto_adjacente):
+    def hipotenusa(self, cateto_oposto, cateto_adjacente):
         return math.sqrt(math.pow(cateto_oposto, 2) + math.pow(cateto_adjacente, 2))
+    
+results = FiguraGeometrica()
+
+area_do_circulo = float(input("Digite o raio para saber a área do círculo: "))
+area1 = results.areaCirculo(area_do_circulo)
+print(f"Área do circulo: {area1} \n")
+
+base = float(input("Digite a base: "))
+altura = float(input("Digite a altura: "))
+area2 = results.areaTriangulo(base, altura)
+print(f"Área do triângulo: {area2} \n")
+
+cat_oposto = float(input("Digite o cateto oposto: "))
+cat_adjacente = float(input("Digite o cateto adjacente: "))
+area3 = results.areaTriangulo(cat_oposto, cat_adjacente)
+print(f"Hipotenusa: {area3}")
---
+++ dia2/desafio4.py
@@ -0,0 +1,11 @@
+class Animal:
+    def falar(self):
+        return "Sons dos animais"
+
+class Gato(Animal):
+    def falar(self):
+        return "Miau"
+    
+class Cachorro(Animal):
+    def falar(self):
+        return "Au Au"
