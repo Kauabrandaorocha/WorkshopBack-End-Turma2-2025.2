ac274dd459e2768da1e47d9f84581863cc68ffd5
kaua <kauavbrandaorocha@gmail.com>
2025-09-11 14:34:40 -0300

    feat

--- dia5/projeto/app/models.py
+++ dia5/projeto/app/models.py
@@ -6,7 +6,7 @@ class EnderecoCep(models.Model):
     bairro = models.CharField(max_length=255)
     cidade = models.CharField(max_length=255)
     estado = models.CharField(max_length=255)
-    cep = models.CharField(max_length=255)
+    cep = models.CharField(max_length=255) 
 
     def __str__(self):
         return f"Rua: {self.logradouro}, Bairro: {self.bairro}, Cidade: {self.localidade}, Estado: {self.uf}, CEP: {self.cep}"
